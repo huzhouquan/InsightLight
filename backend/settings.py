dd34191c22f0cdf8bde12733807dc3be2bee7883
李宇辰 <2657692377@qq.com>
2025-07-08 18:32:39 +0800

    原型

--- backend/settings.py
+++ backend/settings.py
@@ -56,14 +56,14 @@ MIDDLEWARE = [
 ROOT_URLCONF = "backend.urls"
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend') 
+    os.path.join(BASE_DIR, 'static') 
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, 'frontend')],
+        "DIRS": [os.path.join(BASE_DIR, 'static')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
